906d334231ec53d9326e7d07eb993b06dd6dd8a6
gary909 <garywhite61@hotmail.com>
2023-05-08 12:45:51 +0200

    code working

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 function removeUrlAnchor(url){
-    // TODO: complete
+    let myURL = "";
+    for(let i = 0; i < url.length; i++){
+        myURL = myURL + url[i];
+        if(url[i] == '#'){
+            return myURL.slice(0, -1);
+        }
+    }
+    return myURL;
 }
 
 console.log(removeUrlAnchor('www.codewars.com#about')); // 'www.codewars.com'
